03b5e56a43134d590d8a34ac80aa0ffe99aeb5d1
DSCWajahat <66982228+DSCWajahat@users.noreply.github.com>
2020-08-04 11:57:55 +0500

    Add files via upload

---
+++ Pima Indians Diabetes.py
@@ -0,0 +1,83 @@
+
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+25
+26
+27
+28
+29
+30
+31
+32
+33
+34
+35
+36
+37
+38
+39
+40
+41
+# Use scikit-learn to grid search the dropout rate
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import Dropout
+from keras.optimizers import SGD
+from keras.wrappers.scikit_learn import KerasClassifier
+from sklearn.grid_search import GridSearchCV
+import numpy
+import pandas
+# Function to create model, required for KerasClassifier
+def create_model(dropout_rate=0.0):
+	# create model
+	model = Sequential()
+	model.add(Dense(12, input_dim=8, activation='relu'))
+	model.add(Dropout(dropout_rate))
+	model.add(Dense(8, init='normal', activation='relu'))
+	model.add(Dropout(dropout_rate))
+	model.add(Dense(1, init='normal', activation='sigmoid'))
+	# Compile model
+	model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+	return model
+ 
+# fix random seed for reproducibility
+seed = 7
+numpy.random.seed(seed)
+# load dataset
+dataset = numpy.loadtxt("diabetes.csv", delimiter=",")
+# split into input (X) and output (Y) variables
+X = dataset[:,0:8]
+Y = dataset[:,8]
+# create model
+model = KerasClassifier(build_fn=create_model, nb_epoch=100, batch_size=5, verbose=0)
+# grid search dropout rate
+dropout_rate = [0.0, 0.2, 0.5]
+param_grid = dict(dropout_rate=dropout_rate)
+grid = GridSearchCV(estimator=model, param_grid=param_grid, cv=5)
+grid_result = grid.fit(X, Y)
+# summarize results
+print("Best: %f using %s" % (grid_result.best_score_, grid_result.best_params_))
+for params, mean_score, scores in grid_result.grid_scores_:
+    print("%f (%f) with: %r" % (scores.mean(), scores.std(), params))
